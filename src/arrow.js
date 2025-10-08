5a98a9a2fc95afa5695cfe7fc923a3a7367486a0
Anastasiia Maklashevskikh <anastasiia.maklashevskikh@apluss.de>
2025-10-08 17:53:53 +0200

    new consept for arrows

--- src/arrow.js
+++ src/arrow.js
@@ -1,4 +1,4 @@
-import { createSVG } from './svg_utils';
+import { createSVG, $ } from './svg_utils';
 
 export default class Arrow {
     constructor(gantt, from_task, to_task, type) {
@@ -9,129 +9,121 @@ export default class Arrow {
         this.type = type;
         // types: AA, AE, EA, EE
 
-        switch (type) {
-            case "AA":
-                this.calculate_path_type_ss();
-                break;
-            case "EE":
-                this.calculate_path_type_ff();
-                break;
-            case "AE":
-                this.calculate_path_type_sf();
-                break;
-            case "EA":
-                this.calculate_path_type_fs();
-                break;
-        }
-
-        //this.calculate_path();
+        this.calculate_path();
         this.draw();
     }
 
     calculate_path_type_ss() {
         let data = this.get_start_end();
-        const left = data.end_x - 2*this.gantt.options.padding;
-        var offset = 1;
-        if (data.start_y > data.end_y) {
-            offset *= -1;
-        }
-        this.path = `
-                M ${data.start_x} ${data.start_y + offset*6}
+        console.log(data)
+        const left = data.start_x - 0.5 * this.gantt.options.padding;
+        const to_is_below = data.end_y > data.start_y;
+        const to_is_after = data.end_x >= data.start_x;
+        const to_is_below_factor = to_is_below ? 1 : -1;
+
+        if (to_is_after) {
+            this.path = `
+                M ${data.start_x} ${data.start_y + to_is_below_factor * 5}
                 H ${left}
-                V ${data.end_y - offset*4}
-                H ${data.end_x}
-                m -5 -5
-                l 5 5
-                l -5 5`;
+                V ${data.end_y - to_is_below_factor * (1.5 * this.gantt.options.padding)}
+                H ${data.end_x + 0.5 * this.gantt.options.padding}
+                V ${data.end_y - to_is_below_factor * (this.gantt.options.bar_height / 2 + 2)}
+                m ${to_is_below_factor * (-5)}  ${to_is_below_factor * (-5)}
+                l ${to_is_below_factor * (5)}  ${to_is_below_factor * (5)}
+                l ${to_is_below_factor * (5)}  ${to_is_below_factor * (-5)}`;
+        } else {
+            this.path = `
+                M ${data.start_x} ${data.start_y + to_is_below_factor * 5}
+                H ${data.end_x + 0.5 * this.gantt.options.padding}
+                V ${data.end_y - to_is_below_factor * (this.gantt.options.bar_height / 2 + 2)}
+                m ${to_is_below_factor * (-5)}  ${to_is_below_factor * (-5)}
+                l ${to_is_below_factor * (5)}  ${to_is_below_factor * (5)}
+                l ${to_is_below_factor * (5)}  ${to_is_below_factor * (-5)}`;
+        }
     }
 
     calculate_path_type_fs() {
         let data = this.get_start_end();
-        //console.log('[START X, START Y, END X, END Y] ', this.get_start_end())
-        const from_is_below_to = this.from_task.task._index > this.to_task.task._index;
-        
+        const right = data.start_x + 0.5 * this.gantt.options.padding;
+        const to_is_below = data.end_y > data.start_y;
+        const to_is_after = data.end_x >= data.start_x;
+        const to_is_below_factor = to_is_below ? 1 : -1;
 
-        const right = data.start_x + 2*this.gantt.options.padding;
-        const down1 = from_is_below_to ? this.gantt.options.bar_height / 2 - 2 * this.gantt.options.padding : - this.gantt.options.bar_height / 2 + 2* this.gantt.options.padding
-
-        var offset = 1;
-        if (data.start_y > data.end_y) {
-            offset *= -1;
-        }
-
-        if (data.start_x < data.end_x && Math.abs(data.start_x - data.end_x) > 2*this.gantt.options.padding) {
+        if (!to_is_after) {
             this.path = `
-                M ${data.start_x} ${data.start_y + offset*6}
+                M ${data.start_x} ${data.start_y + to_is_below_factor * 5}
                 H ${right}
-                V ${data.end_y - offset*4}
-                H ${data.end_x}
-                m -5 -5
-                l 5 5
-                l -5 5`;
+                V ${data.end_y - to_is_below_factor * (1.5 * this.gantt.options.padding)}
+                H ${data.end_x + 0.5 * this.gantt.options.padding}
+                V ${data.end_y - to_is_below_factor * (this.gantt.options.bar_height / 2 + 2)}
+                m ${to_is_below_factor * (-5)}  ${to_is_below_factor * (-5)}
+                l ${to_is_below_factor * (5)}  ${to_is_below_factor * (5)}
+                l ${to_is_below_factor * (5)}  ${to_is_below_factor * (-5)}`;
         } else {
             this.path = `
-                M ${data.start_x} ${data.start_y + offset*6}
-                H ${right}
-                v ${down1}
-                H ${data.end_x - 2* this.gantt.options.padding}
-                V ${data.end_y - offset*4}
-                H ${data.end_x}
-                m -5 -5
-                l 5 5
-                l -5 5`;
+                M ${data.start_x} ${data.start_y + to_is_below_factor * 5}
+                H ${data.end_x + 0.5 * this.gantt.options.padding}
+                V ${data.end_y - to_is_below_factor * (this.gantt.options.bar_height / 2 + 2)}
+                m ${to_is_below_factor * (-5)}  ${to_is_below_factor * (-5)}
+                l ${to_is_below_factor * (5)}  ${to_is_below_factor * (5)}
+                l ${to_is_below_factor * (5)}  ${to_is_below_factor * (-5)}`;
         }
-
     }
+
     calculate_path_type_ff() {
         let data = this.get_start_end();
-        const right = data.end_x + 2*this.gantt.options.padding;
-        var offset = 1;
-        if (data.start_y > data.end_y) {
-            offset *= -1;
-        }
+        console.log(data)
+        const right = data.start_x + 0.5 * this.gantt.options.padding;
+        const to_is_below = data.end_y > data.start_y;
+        const to_is_after = data.end_x > data.start_x;
+        const to_is_below_factor = to_is_below ? 1 : -1;
 
-        this.path = `
-                M ${data.start_x} ${data.start_y + offset*6}
+        if (!to_is_after) {
+            this.path = `
+                M ${data.start_x} ${data.start_y + to_is_below_factor * 5}
                 H ${right}
-                V ${data.end_y - offset*4}
-                H ${data.end_x}
-                m 5 -5
-                l -5 5
-                l 5 5`;
+                V ${data.end_y - to_is_below_factor * (1.5 * this.gantt.options.padding)}
+                H ${data.end_x - 0.5 * this.gantt.options.padding}
+                V ${data.end_y - to_is_below_factor * (this.gantt.options.bar_height / 2 + 2)}
+                m ${to_is_below_factor * (-5)}  ${to_is_below_factor * (-5)}
+                l ${to_is_below_factor * (5)}  ${to_is_below_factor * (5)}
+                l ${to_is_below_factor * (5)}  ${to_is_below_factor * (-5)}`;
+        } else {
+            this.path = `
+                M ${data.start_x} ${data.start_y + to_is_below_factor * 6}
+                H ${data.end_x - 0.5 * this.gantt.options.padding}
+                V ${data.end_y - to_is_below_factor * (this.gantt.options.bar_height / 2 + 2)}
+                m ${to_is_below_factor * (-5)}  ${to_is_below_factor * (-5)}
+                l ${to_is_below_factor * (5)}  ${to_is_below_factor * (5)}
+                l ${to_is_below_factor * (5)}  ${to_is_below_factor * (-5)}`;
+        }
 
     }
     calculate_path_type_sf() {
         let data = this.get_start_end();
-        const from_is_below_to = this.from_task.task._index > this.to_task.task._index;
+        const left = data.start_x - 0.5 * this.gantt.options.padding;
+        const to_is_below = data.end_y > data.start_y;
+        const to_is_after = data.end_x > data.start_x;
+        const to_is_below_factor = to_is_below ? 1 : -1;
 
-        const left = data.start_x - 2*this.gantt.options.padding;
-        const down1 = from_is_below_to ? this.gantt.options.bar_height / 2 - 2 * this.gantt.options.padding : -this.gantt.options.bar_height / 2 + 2* this.gantt.options.padding
-
-         var offset = 1;
-        if (data.start_y > data.end_y) {
-            offset *= -1;
-        }
-
-        if (data.start_x >= data.end_x && Math.abs(data.start_x - data.end_x) > 2*this.gantt.options.padding) {
+        if (to_is_after) {
             this.path = `
-                M ${data.start_x} ${data.start_y + offset*5}
+                M ${data.start_x} ${data.start_y + to_is_below_factor * 5}
                 H ${left}
-                V ${data.end_y - offset*5}
-                H ${data.end_x}
-                m 5 -5
-                l -5 5
-                l 5 5`;
+                V ${data.end_y - to_is_below_factor * (1.5 * this.gantt.options.padding)}
+                H ${data.end_x - 0.5 * this.gantt.options.padding}
+                V ${data.end_y - to_is_below_factor * (this.gantt.options.bar_height / 2 + 2)}
+                m ${to_is_below_factor * (-5)}  ${to_is_below_factor * (-5)}
+                l ${to_is_below_factor * (5)}  ${to_is_below_factor * (5)}
+                l ${to_is_below_factor * (5)}  ${to_is_below_factor * (-5)}`;
         } else {
             this.path = `
-                M ${data.start_x} ${data.start_y + offset*5}
-                H ${left}
-                v ${down1}
-                H ${data.end_x + 2* this.gantt.options.padding}
-                V ${data.end_y - offset*5}
-                H ${data.end_x}
-                m 5 -5
-                l -5 5
-                l 5 5`;
+                M ${data.start_x} ${data.start_y + to_is_below_factor * 5}
+                H ${data.end_x - 0.5 * this.gantt.options.padding}
+                V ${data.end_y - to_is_below_factor * (this.gantt.options.bar_height / 2 + 2)}
+                m ${to_is_below_factor * (-5)}  ${to_is_below_factor * (-5)}
+                l ${to_is_below_factor * (5)}  ${to_is_below_factor * (5)}
+                l ${to_is_below_factor * (5)}  ${to_is_below_factor * (-5)}`;
         }
 
     }
@@ -145,32 +137,32 @@ export default class Arrow {
         if (this.type == "AA" || this.type == "AE") {
             start_x = this.from_task.$bar.getX();
         } else {
-            start_x = this.from_task.$bar.getX() + this.from_task.$bar.getWidth() - 3;
+            start_x = this.from_task.$bar.getX() + this.from_task.$bar.getWidth();
         }
 
         if (this.type == "AA" || this.type == "EA") {
-            end_x = this.to_task.$bar.getX() - 3;
+            end_x = this.to_task.$bar.getX();
         } else {
-            end_x = this.to_task.$bar.getX() + this.to_task.$bar.getWidth() + 3;
+            end_x = this.to_task.$bar.getX() + this.to_task.$bar.getWidth();
         }
-        
+
         start_y =
             this.gantt.config.header_height +
             this.gantt.options.bar_height +
             (this.gantt.options.padding + this.gantt.options.bar_height) *
-                this.from_task.task._index +
-            this.gantt.options.padding / 2 - this.gantt.options.bar_height/2;
+            this.from_task.task._index +
+            this.gantt.options.padding / 2 - this.gantt.options.bar_height / 2;
 
         end_y =
             this.gantt.config.header_height +
             this.gantt.options.bar_height / 2 +
             (this.gantt.options.padding + this.gantt.options.bar_height) *
-                this.to_task.task._index +
+            this.to_task.task._index +
             this.gantt.options.padding / 2;
 
         //console.log(start_x, start_y, end_x, end_y)
-        
-        return {start_x: start_x, start_y: start_y, end_x: end_x, end_y: end_y};
+
+        return { start_x: start_x, start_y: start_y, end_x: end_x, end_y: end_y };
     }
 
     draw() {
@@ -179,6 +171,42 @@ export default class Arrow {
             'data-from': this.from_task.task.id,
             'data-to': this.to_task.task.id,
         });
+        this.element.addEventListener("mouseover", (e) => {
+            //console.log(this.from_task.task, this.to_task.task)
+            console.log(this.gantt.bars)
+            const fromTask = this.gantt.bars.find(b => b.task.id == this.from_task.task.id).$bar;
+            const toTask = this.gantt.bars.find(b => b.task.id == this.to_task.task.id).$bar;
+            this.element.setAttribute("stroke", "#99004d");
+            fromTask.style.stroke =  "#99004d";
+            fromTask.style.strokeWidth = 1.5;
+            toTask.style.stroke = "#99004d";
+            toTask.style.strokeWidth = 1.5;
+        });
+        this.element.addEventListener("mouseleave", (e) => {
+            console.log("MOUSELEAVE")
+            const fromTask = this.gantt.bars.find(b => b.task.id == this.from_task.task.id).$bar;
+            const toTask = this.gantt.bars.find(b => b.task.id == this.to_task.task.id).$bar;
+            this.element.setAttribute("stroke", "black");
+            fromTask.style.strokeWidth = 0;
+            toTask.style.strokeWidth = 0;
+        });
+    }
+
+    calculate_path() {
+        switch (this.type) {
+            case "AA":
+                this.calculate_path_type_ss();
+                break;
+            case "EE":
+                this.calculate_path_type_ff();
+                break;
+            case "AE":
+                this.calculate_path_type_sf();
+                break;
+            case "EA":
+                this.calculate_path_type_fs();
+                break;
+        }
     }
 
     update() {
